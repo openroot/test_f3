9deef803b00a150493499d7c262ddd9010e4e16a
openroot <dev.openroot@gmail.com>
2023-06-13 06:06:59 +0530

    Trying GET, POST in same route handler

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 require(__DIR__ . '/../vendor/autoload.php');
 
 $fff = Base::instance();
-$fff->route('GET /', function ($fff) {
-	echo "Hello World!";
+$fff->route('GET|POST /helloworld', function ($fff) {
+	echo 'Hello World! This is a '.$fff->VERB.'.';
 });
 $fff->run();
