f93424c12f1726f0c3d8641168069c248faf2b86
openroot <dev.openroot@gmail.com>
2023-06-15 19:17:46 +0530

    sampling 'templating with raw php'.

--- public/index.php
+++ public/index.php
@@ -7,4 +7,13 @@ $f3->set('AUTOLOAD', '../app/');
 
 $f3->route('GET|POST /helloworld/@name/@age/@profession', 'operations\operation_index->helloworld_default');
 
+$f3->route(
+	'GET /',
+	function ($f3) {
+		$f3->set('value1', 'SAMPLING');
+		$view = new View();
+		echo $view->render('sample_template.htm');
+	}
+);
+
 $f3->run();
