12494e791da1e5a11000886ec38e12afafa86ce7
openroot <dev.openroot@gmail.com>
2023-06-16 14:01:52 +0530

    adding 'named routes'.

--- app/operations/operation_index.php
+++ app/operations/operation_index.php
@@ -4,13 +4,13 @@ namespace operations;
 
 class operation_index {
 	public function helloworld_default(\Base $f3): void {
-		echo 'Hello World! This is a '.$f3->get('VERB').'.';
+		echo 'Hello World! This is a '.$f3->VERB.'.';
 
 		// URI example
 		// http://localhost:4000/helloworld/D Tapader/34/Software Engineer
 		echo '<br><br>Query string posted:';
-		echo '<br>Name = '.$f3->get('PARAMS.name');
-		echo '<br>Age = '.$f3->get('PARAMS.age');
-		echo '<br>Profession = '.$f3->get('PARAMS.profession');
+		echo '<br>Name = '.$f3['PARAMS.name'];
+		echo '<br>Age = '.$f3['PARAMS.age'];
+		echo '<br>Profession = '.$f3['PARAMS.profession'];
 	}
 }
--- public/index.php
+++ public/index.php
@@ -2,17 +2,15 @@
 
 require(__DIR__ . '/../vendor/autoload.php');
 
-$f3 = Base::instance();
+$f3 = \Base::instance();
 $f3->set('AUTOLOAD', '../app/');
 
-$f3->route('GET|POST /helloworld/@name/@age/@profession', 'operations\operation_index->helloworld_default');
-
-$f3->route(
-	'GET /',
-	function ($f3) {
-		$f3->set('value1', 'SAMPLING');
+$f3->route('GET @index: /', function ($f3) {
+		$f3->value1='A user-defined value.';
 		echo \Template::instance()->render('/../app/segments/segment_sample.htm');
 	}
 );
+
+$f3->route('GET|POST @index: /helloworld/@name/@age/@profession', 'operations\operation_index->helloworld_default');
 
 $f3->run();
