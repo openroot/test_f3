2c50bccfb11d393154e2d27e22b6ff7e1b3e10cc
openroot <dev.openroot@gmail.com>
2023-06-16 13:00:15 +0530

    getting f3 variable values appropriate syntaxing

--- app/operations/operation_index.php
+++ app/operations/operation_index.php
@@ -3,14 +3,14 @@
 namespace operations;
 
 class operation_index {
-	public function helloworld_default(\Base $f3, array $args = []): void {
-		echo 'Hello World!! This is a '.$f3->VERB.'.';
+	public function helloworld_default(\Base $f3): void {
+		echo 'Hello World! This is a '.$f3->get('VERB').'.';
 
 		// URI example
 		// http://localhost:4000/helloworld/D Tapader/34/Software Engineer
 		echo '<br><br>Query string posted:';
-		echo '<br>Name = '.$args['name'];
-		echo '<br>Age = '.$args['age'];
-		echo '<br>Profession = '.$args['profession'];
+		echo '<br>Name = '.$f3->get('PARAMS.name');
+		echo '<br>Age = '.$f3->get('PARAMS.age');
+		echo '<br>Profession = '.$f3->get('PARAMS.profession');
 	}
 }
